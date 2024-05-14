fceb283adc17e1c4dfeaecdacefebdd6ec4fdcd0
Siddharth Maddheshiya <bhaisiddharth63@gmail.com>
2024-05-14 12:24:11 -0700

    merge

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import { useState } from 'react'
-import PasswordGenerator from './components/PasswordGenerator'
-// import BgChanger from './components/01BgChanger'
-
+import { useState } from "react";
+import PasswordGenerator from "./components/PasswordGenerator";
+import BgChanger from "./components/01BgChanger";
 
 function App() {
-  // const [color, setColor]=useState("olive")
+  const [color, setColor] = useState("olive");
 
-  // const handleColor = ()=>{
-  //   setColor()
-  // }
-
+  const handleColor = () => {
+    setColor();
+  };
 
   return (
+    <>
+      <div
+        className="w-full max-h-screen md:h-screen bg-blue-700 relative"
+        style={{ backgroundColor: color }}
+      >
+        <BgChanger setColor={setColor} />
+      </div>
 
-    //bgChanger code------ to run the code plz comment out ---------
-  //   <div className='w-full max-h-screen md:h-screen bg-blue-700 relative' style={{backgroundColor:color}}>
-  // <BgChanger setColor={setColor}/>
-  //   </div>
-
-  // -------------------------
-
-  <div className='w-full h-screen bg-black fixed'>
-    <PasswordGenerator/>
-  </div>
-  )
+      <div className="w-full h-screen bg-black pt-10">
+        <PasswordGenerator />
+      </div>
+    </>
+  );
 }
 
-export default App
+export default App;
